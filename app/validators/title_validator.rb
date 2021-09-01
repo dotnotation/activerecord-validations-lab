a120e5ad394647cc3d25ca09ea45b35ce891b738
dotnotation <dotinmotionproductions@gmail.com>
2021-09-01 13:48:41 -0400

    commit tree 91fc384c2e9a34c799feb7f4377296dbfd292154

---
+++ app/validators/title_validator.rb
@@ -0,0 +1,8 @@
+class TitleValidator < ActiveModel::Validator
+
+    def validate(record)
+        unless record.title.match?(/Won't Believe|Secret|Guess|Top \d/)
+            record.errors[:title] << "Your title isn't click-bait-y enough!"
+        end
+    end
+end
